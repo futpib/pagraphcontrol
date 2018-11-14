c3deec41d7f7c7cbd388df456bc9a3c1ded5a680
futpib <futpib@gmail.com>
2018-11-14 05:37:09 +0300

    Fix focus stealing

--- components/button/index.js
+++ components/button/index.js
@@ -1,12 +1,18 @@
 
+const {
+	memoizeWith,
+} = require('ramda');
+
 const r = require('r-dom');
 
+const ref = memoizeWith(autoFocus => String(Boolean(autoFocus)), autoFocus => input => {
+	if (input && autoFocus) {
+		input.focus();
+	}
+});
+
 const Button = props => r.button({
-	ref: input => {
-		if (input && props.autoFocus) {
-			input.focus();
-		}
-	},
+	ref,
 	className: 'button',
 	...props,
 }, props.children);
